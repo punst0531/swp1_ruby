448eaa9767501af63c6d3b1a534435c8eb05a08a
punst0531 <punst0531@gmail.com>
2015-09-24 07:06:32 +0000

    nokogiri

---
+++ nokogiri.rb
@@ -0,0 +1,28 @@
+require 'nokogiri'
+require 'open-uri'
+
+PAGE_URL = "http://joongang.joins.com/"
+
+page = Nokogiri::HTML(open(PAGE_URL))
+
+
+link_tags = page.css('.main_article li a') 
+link_tags.each do |link|
+    puts "타이틀 : " + link.text
+
+    newUrl = link['href'].to_s
+    
+    page = Nokogiri::HTML(open(newUrl))
+
+    articles = page.css('.article_body')
+
+    mainbody = []
+
+    articles.each do |article|
+    mainbody.push article.text
+    end
+
+    puts mainbody
+
+    
+end
